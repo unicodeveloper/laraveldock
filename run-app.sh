f430612bd362f8661020cd27396490de7a1b982e
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-10-02 22:57:43 +0100

    php-fpm

--- run-app.sh
+++ run-app.sh
@@ -15,4 +15,4 @@ php artisan view:cache
 php artisan migrate --force
 
 # Run the app
-node /assets/scripts/prestart.mjs /assets/nginx.template.conf  /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
+php-fpm
